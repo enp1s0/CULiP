19d357c0c1900529724f402d9bf50311e02a250e
mutsuki <mutsuki@momo86.net>
2021-05-03 13:36:26 +0900

    Add cublas profiling controler

--- src/cublas.cu
+++ src/cublas.cu
@@ -4,11 +4,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <CULiP/cublas.hpp>
 #include "utils.hpp"
 
+extern "C" {
+// dlopen cache
 void* CULiP_cublas_lib_handle_cache = NULL;
 
-extern "C" {
+// Control profiling
+// 0         = Profiling
+// Otherwise = Not profiling
+int CULiP_profiling_control_array[CULiP_cublas_enum_length] = {0};
+
+// Controler setter
+void CULiP_profile_cublas_enable(const CULiP_cublas_control_t target_function) {
+	CULiP_profiling_control_array[target_function] = 0;
+}
+void CULiP_profile_cublas_disable(const CULiP_cublas_control_t target_function) {
+	CULiP_profiling_control_array[target_function] = 1;
+}
+void CULiP_profile_cublas_enable_all() {
+	for (unsigned target_function = 0; target_function < CULiP_cublas_enum_length; target_function++) {
+		CULiP_profiling_control_array[target_function] = 0;
+	}
+}
+void CULiP_profile_cublas_disable_all() {
+	for (unsigned target_function = 0; target_function < CULiP_cublas_enum_length; target_function++) {
+		CULiP_profiling_control_array[target_function] = 1;
+	}
+}
+
 cublasStatus_t cublasSgemm(cublasHandle_t handle, cublasOperation_t transa,
                            cublasOperation_t transb, int m, int n, int k,
                            const float *alpha, const float *A, int lda,
@@ -19,26 +44,31 @@ cublasStatus_t cublasSgemm(cublasHandle_t handle, cublasOperation_t transa,
 	cublasStatus_t (*cublas_lib_func)(cublasHandle_t, cublasOperation_t, cublasOperation_t, int, int, int, const float*, const float*, int, const float*, int, const float*, float*, int);
 	*(void**)(&cublas_lib_func) = CULiP_get_function_pointer("CULIP_CUBLAS_LIB_PATH", __func__, &CULiP_cublas_lib_handle_cache);
 
-	// Get current cuda stream
 	cudaStream_t cuda_stream;
-	cublasGetStream(handle, &cuda_stream);
-
-	// Profile result structure
 	struct CULiP_profile_result profile_result;
-	snprintf(profile_result.function_name, profile_result.function_name_length - 1, "%s-m%d-n%d-k%d", __func__, m, n ,k);
 
-	// Record start rimestamp
-	CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.start_timestamp);
+	if (CULiP_profiling_control_array[CULiP_cublasSgemm] == 0) {
+		// Get current cuda stream
+		cublasGetStream(handle, &cuda_stream);
+
+		// Profile result structure
+		snprintf(profile_result.function_name, profile_result.function_name_length - 1, "%s-m%d-n%d-k%d", __func__, m, n ,k);
+
+		// Record start rimestamp
+		CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.start_timestamp);
+	}
 
 	// Call the function
 	const cublasStatus_t result = (*cublas_lib_func)(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 	CULIBPROFILER_DEBUG_PRINT(printf("[CULiP Debug][%s] executed\n", __func__));
 
-	// Record end rimestamp
-	CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.end_timestamp);
+	if (CULiP_profiling_control_array[CULiP_cublasSgemm] == 0) {
+		// Record end rimestamp
+		CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.end_timestamp);
 
-	// Print result
-	CULiP_launch_function(cuda_stream, &CULiP_print_profile_result, (void*)&profile_result);
+		// Print result
+		CULiP_launch_function(cuda_stream, &CULiP_print_profile_result, (void*)&profile_result);
+	}
 
 	return result;
 }
@@ -53,26 +83,31 @@ cublasStatus_t cublasDgemm(cublasHandle_t handle, cublasOperation_t transa,
 	cublasStatus_t (*cublas_lib_func)(cublasHandle_t, cublasOperation_t, cublasOperation_t, int, int, int, const double*, const double*, int, const double*, int, const double*, double*, int);
 	*(void**)(&cublas_lib_func) = CULiP_get_function_pointer("CULIP_CUBLAS_LIB_PATH", __func__, &CULiP_cublas_lib_handle_cache);
 
-	// Get current cuda stream
 	cudaStream_t cuda_stream;
-	cublasGetStream(handle, &cuda_stream);
-
-	// Profile result structure
 	struct CULiP_profile_result profile_result;
-	snprintf(profile_result.function_name, profile_result.function_name_length - 1, "%s-m%d-n%d-k%d", __func__, m, n ,k);
 
-	// Record start rimestamp
-	CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.start_timestamp);
+	if (CULiP_profiling_control_array[CULiP_cublasDgemm] == 0) {
+		// Get current cuda stream
+		cublasGetStream(handle, &cuda_stream);
+
+		// Profile result structure
+		snprintf(profile_result.function_name, profile_result.function_name_length - 1, "%s-m%d-n%d-k%d", __func__, m, n ,k);
+
+		// Record start rimestamp
+		CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.start_timestamp);
+	}
 
 	// Call the function
 	const cublasStatus_t result = (*cublas_lib_func)(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 	CULIBPROFILER_DEBUG_PRINT(printf("[CULiP Debug][%s] executed\n", __func__));
 
-	// Record end rimestamp
-	CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.end_timestamp);
+	if (CULiP_profiling_control_array[CULiP_cublasDgemm] == 0) {
+		// Record end rimestamp
+		CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.end_timestamp);
 
-	// Print result
-	CULiP_launch_function(cuda_stream, &CULiP_print_profile_result, (void*)&profile_result);
+		// Print result
+		CULiP_launch_function(cuda_stream, &CULiP_print_profile_result, (void*)&profile_result);
+	}
 
 	return result;
 }
@@ -87,26 +122,31 @@ cublasStatus_t cublasHgemm(cublasHandle_t handle, cublasOperation_t transa,
 	cublasStatus_t (*cublas_lib_func)(cublasHandle_t, cublasOperation_t, cublasOperation_t, int, int, int, const half*, const half*, int, const half*, int, const half*, half*, int);
 	*(void**)(&cublas_lib_func) = CULiP_get_function_pointer("CULIP_CUBLAS_LIB_PATH", __func__, &CULiP_cublas_lib_handle_cache);
 
-	// Get current cuda stream
 	cudaStream_t cuda_stream;
-	cublasGetStream(handle, &cuda_stream);
-
-	// Profile result structure
 	struct CULiP_profile_result profile_result;
-	snprintf(profile_result.function_name, profile_result.function_name_length - 1, "%s-m%d-n%d-k%d", __func__, m, n ,k);
 
-	// Record start rimestamp
-	CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.start_timestamp);
+	if (CULiP_profiling_control_array[CULiP_cublasHgemm] == 0) {
+		// Get current cuda stream
+		cublasGetStream(handle, &cuda_stream);
+
+		// Profile result structure
+		snprintf(profile_result.function_name, profile_result.function_name_length - 1, "%s-m%d-n%d-k%d", __func__, m, n ,k);
+
+		// Record start rimestamp
+		CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.start_timestamp);
+	}
 
 	// Call the function
 	const cublasStatus_t result = (*cublas_lib_func)(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 	CULIBPROFILER_DEBUG_PRINT(printf("[CULiP Debug][%s] executed\n", __func__));
 
-	// Record end rimestamp
-	CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.end_timestamp);
+	if (CULiP_profiling_control_array[CULiP_cublasHgemm] == 0) {
+		// Record end rimestamp
+		CULiP_launch_function(cuda_stream, &CULiP_record_timestamp, (void*)&profile_result.end_timestamp);
 
-	// Print result
-	CULiP_launch_function(cuda_stream, &CULiP_print_profile_result, (void*)&profile_result);
+		// Print result
+		CULiP_launch_function(cuda_stream, &CULiP_print_profile_result, (void*)&profile_result);
+	}
 
 	return result;
 }
