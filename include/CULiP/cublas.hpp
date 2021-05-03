dbb39d739c7f17c1163a0ddadc43b680f7ebc47c
mutsuki <mutsuki@momo86.net>
2021-05-03 13:36:42 +0900

    Add include/CULiP/cublas.hpp

---
+++ include/CULiP/cublas.hpp
@@ -0,0 +1,15 @@
+#ifndef __CULIP_CUBLAS_HPP__
+#define __CULIP_CUBLAS_HPP__
+
+enum CULiP_cublas_control_t {
+	CULiP_cublasDgemm = 0,
+	CULiP_cublasSgemm = 1,
+	CULiP_cublasHgemm = 2,
+	CULiP_cublas_enum_length
+};
+
+extern "C" void CULiP_profile_cublas_enable(const CULiP_cublas_control_t target_function);
+extern "C" void CULiP_profile_cublas_disable(const CULiP_cublas_control_t target_function);
+extern "C" void CULiP_profile_cublas_enable_all();
+extern "C" void CULiP_profile_cublas_disable_all();
+#endif
