1e676834d602f1a2d408806483de5904b82088b6
mutsuki <mutsuki@momo86.net>
2021-05-05 12:39:35 +0900

    Add a test for `cublasCgemm`

--- tests/cublas_test.cu
+++ tests/cublas_test.cu
@@ -39,6 +39,14 @@ cublasStatus_t gemm<half  , op_gemm>(cublasHandle_t handle, cublasOperation_t tr
                            int ldc) {
 	return cublasHgemm(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 }
+template <>
+cublasStatus_t gemm<cuComplex, op_gemm>(cublasHandle_t handle, cublasOperation_t transa,
+                           cublasOperation_t transb, int m, int n, int k,
+                           const cuComplex *alpha, const cuComplex *A, int lda,
+                           const cuComplex *B, int ldb, const cuComplex *beta, cuComplex *C,
+                           int ldc) {
+	return cublasCgemm(handle, transa, transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+}
 // -----------------------------------------------------
 // op_gemmEx
 // -----------------------------------------------------
@@ -67,11 +75,16 @@ cublasStatus_t gemm<half  , op_gemmEx>(cublasHandle_t handle, cublasOperation_t 
 	return cublasGemmEx(handle, transa, transb, m, n, k, alpha, A, CUDA_R_16F, lda, B, CUDA_R_16F, ldb, beta, C, CUDA_R_16F, ldc, CUDA_R_16F, CUBLAS_GEMM_DEFAULT);
 }
 
+template <class T>
+T convert(const double a) {return static_cast<T>(a);}
+template <> cuComplex       convert<cuComplex      >(const double a) {return make_float2(a, 0);}
+template <> cuDoubleComplex convert<cuDoubleComplex>(const double a) {return make_double2(a, 0);}
+
 template <class T, class Op>
 void gemm_test() {
 	const std::size_t n = 1lu << 10;
-	const auto alpha = static_cast<T>(1);
-	const auto beta  = static_cast<T>(0);
+	const auto alpha = convert<T>(1);
+	const auto beta  = convert<T>(0);
 
 	T* mat_a;
 	T* mat_b;
@@ -105,6 +118,7 @@ void test_all() {
 	gemm_test<double, op_gemm  >();
 	gemm_test<float , op_gemm  >();
 	gemm_test<half  , op_gemm  >();
+	gemm_test<cuComplex, op_gemm  >();
 	gemm_test<double, op_gemmEx>();
 	gemm_test<float , op_gemmEx>();
 	gemm_test<half  , op_gemmEx>();
